c5490c64924efbdb4ac11c5ebd669c14b51ec23b
h4nb31 <d3xh4ck3r@gmail.com>
2023-10-25 18:52:08 +0300

    get list of jobs (names and id's)

--- conf.py
+++ conf.py
@@ -1,4 +1,8 @@
 TOKEN = "6043156274:AAFIH9reU_iFCHCLb5zJePyREG7N6moumHc"
-Message1 = "@Salomatin_Evgeny_Posadmin\n@dpetelin_posdamin\n@Konstantin_Pushkin_POSADMIN\nНе забываем проверить что смены в Stars закрыты\nИ отпишитесь Дмитрию в чат Stars_IT"
-Message2 = "@Salomatin_Evgeny_Posadmin\n@dpetelin_posdamin\n@Konstantin_Pushkin_POSADMIN\nПроверили закрытие смен в Stars?"
-Message3 ="@Salomatin_Evgeny_Posadmin\n@dpetelin_posdamin\n@Konstantin_Pushkin_POSADMIN\nТочно проверили закрытие смен в Stars?\nОтправили отчет Дмитрию?"
+Message1 = "Не забываем проверить что смены в Stars закрыты\nИ отпишитесь Дмитрию в чат Stars_IT"
+Message2 = "Проверили закрытие смен в Stars?"
+Message3 ="Точно проверили закрытие смен в Stars?\nОтправили отчет Дмитрию?"
+
+Message_D = "@dpetelin_posdamin\n"
+Message_P = "@Konstantin_Pushkin_POSADMIN\n"
+Message_S = "@Salomatin_Evgeny_Posadmin\n"
--- main.py
+++ main.py
@@ -2,22 +2,23 @@
 import logging
 import conf
 import datetime
+from apscheduler import schedulers
 import time
 from datetime import time
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, JobQueue
+from telegram.ext import Job
 import pytz
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.ERROR
+    level=logging.INFO
 )
 
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    #now = time.strftime("%H:%M:%S")
     try:
         print (await context.bot.get_chat(chat_id = update.effective_chat.id))
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Chat Info sended")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Ифнормация по чату отправлена")
     except Exception as e:
            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Exception: {e}")
            print(f"exception: {e}")
@@ -25,34 +26,60 @@ async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
 #async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
 #    await context.bot.send_message(chat_id="-569283112", text="Test message evry 30 seconds")
 
-async def callback_daily(context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id="6017481524", text=conf.Message1 )
+def user_input(u_input: str):
+    return u_input
 
-async def callback_daily1(context: ContextTypes.DEFAULT_TYPE):
+async def Daily_ping(context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id="6017481524", text=conf.Message_P + conf.Message1 )
+
+async def Daily_ping_repeat_1(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id="6017481524", text=conf.Message2 )
 
-async def callback_daily2(context: ContextTypes.DEFAULT_TYPE):
+async def Daily_ping_repeat_2(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id="6017481524", text=conf.Message3 )
 
-
+async def get_jobs(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    job_name = [Job.name for Job in context.job_queue.jobs()]
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Все работающие задания: \n\n" + str(job_name)) 
+    
+async def remove_job(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        job_name = [Job.name for Job in context.job_queue.jobs()]
+        job_id = [Job.id for Job in context.job_queue.jobs()]
+        job_len = len(job_name)
+        for i in job_name:
+            if (job_name[i] == context.args[0]):
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=job_name[i])
+                context.job_queue.scheduler.remove_job(str(job_id[i]))
+                await context.bot.send_message(chat_id=update.effective_chat.id, text="Job: " + str(job_name[i]) +"\nStatus: Deleted")
+        ##context.job_queue.scheduler.remove_job()
+    except Exception as e:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Exception: {e}")
+        print(context.job_queue.scheduler.get_jobs())
+        print(type(job_name))
+    
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(conf.TOKEN).build()
     job_queue = application.job_queue
     
     local_timezone = pytz.timezone('Europe/Moscow')
-    target_time = datetime.time(18, 41, 40, tzinfo=local_timezone)
+    target_time = datetime.time(21, 2, 40, tzinfo=local_timezone)
     target_time1 = datetime.time(18, 42, 00, tzinfo=local_timezone)
     target_time2 = datetime.time(18, 42, 40, tzinfo=local_timezone)
     target_days = 0,1,2,3,4,5,6
-    #job_minute = job_queue.run_repeating(callback_minute, interval=30, first=5)
-    job_daily = job_queue.run_daily(callback_daily, time=target_time, days=target_days)
-    job_daily1 = job_queue.run_daily(callback_daily1, time=target_time1, days=target_days)
-    job_daily2 = job_queue.run_daily(callback_daily2, time=target_time2, days=target_days)
-  
     
-    info_handler = CommandHandler('sent_info', info)
+    #Constant jobs
+    job_daily = job_queue.run_daily(Daily_ping, time=target_time, days=target_days)
+    job_daily = job_queue.run_daily(Daily_ping_repeat_1, time=target_time1, days=target_days)
+    job_daily = job_queue.run_daily(Daily_ping_repeat_2, time=target_time2, days=target_days)
+  
+    jobs_handler = CommandHandler('jobs', get_jobs)
+    info_handler = CommandHandler('send_info', info)
+    remove_handler = CommandHandler('remove_job',remove_job)
     application.add_handler(info_handler)
+    application.add_handler(jobs_handler)
+    application.add_handler(remove_handler)
    
     
     
